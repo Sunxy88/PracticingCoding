ae48c742c925b832b9f6c86a09b36a7fe407e6cd
Xi SONG <sungxisx@icloud.com>
2020-04-13 15:21:52 +0200

    Initial commit

---
+++ src/main/java/Algorithm/MainDrive.java
@@ -0,0 +1,13 @@
+package Algorithm;
+
+public class MainDrive {
+    public static void main(String[] args) {
+        Solution.helloWorld();
+    }
+}
+
+class Solution {
+    public static void helloWorld() {
+        System.out.println("Hello World");
+    }
+}
