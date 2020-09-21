55d65d06bd69fbe8c97b5ee491b6ca97b542ba83
Xi SONG <sungxisx@icloud.com>
2020-09-21 09:54:10 +0200

    LeetCode 538

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode538.java
@@ -0,0 +1,18 @@
+package Algorithm.Solutions.LeetCode;
+
+import Algorithm.UsefulDataStructure.TreeNode;
+
+public class LeetCode538 {
+
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
