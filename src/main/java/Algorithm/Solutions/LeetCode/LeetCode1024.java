bf962e54377bb2c82a5b0617ea139a92c8a128fd
Xi SONG <sungxisx@icloud.com>
2020-10-24 00:05:09 +0200

    Second solution of LeetCode 1024. Happy 1024!

--- src/main/java/Algorithm/Solutions/LeetCode/LeetCode1024.java
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode1024.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public class LeetCode1024 {
 
+//    DP
     public int videoStitching(int[][] clips, int T) {
         int[] dp = new int[T + 1];
         Arrays.fill(dp, Integer.MAX_VALUE - 1);
@@ -17,5 +18,27 @@ public class LeetCode1024 {
         }
         return dp[T] == Integer.MAX_VALUE - 1 ? -1 : dp[T];
     }
+
+//    Greedy
+//    public int videoStitching(int[][] clips, int T) {
+//        int[] maxn = new int[T];
+//        for (int[] clip : clips) {
+//            if (clip[0] < T) {
+//                maxn[clip[0]] = Math.max(maxn[clip[0]], clip[1]);
+//            }
+//        }
+//        int maximum = 0, res = 0, lastPos = 0;
+//        for (int i = 0; i < T; i++) {
+//            maximum = Math.max(maximum, maxn[i]);
+//            if (maximum == i) {
+//                return -1;
+//            }
+//            if (i == lastPos) {
+//                res++;
+//                lastPos = maximum;
+//            }
+//        }
+//        return res;
+//    }
 
 }
