e44a13b2fcea3d1a068634be80761fec40a904f2
Xi SONG <sungxisx@icloud.com>
2020-10-23 23:50:12 +0200

    LeetCode 1024. Happy 1024!

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode1024.java
@@ -0,0 +1,21 @@
+package Algorithm.Solutions.LeetCode;
+
+import java.util.Arrays;
+
+public class LeetCode1024 {
+
+    public int videoStitching(int[][] clips, int T) {
+        int[] dp = new int[T + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE - 1);
+        dp[0] = 0;
+        for (int i = 1; i <= T; i++) {
+            for (int[] clip : clips) {
+                if (clip[0] < i && i <= clip[1]) {
+                    dp[i] = Math.min(dp[i], dp[clip[0]] + 1);
+                }
+            }
+        }
+        return dp[T] == Integer.MAX_VALUE - 1 ? -1 : dp[T];
+    }
+
+}
