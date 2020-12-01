d1772680be2621e0d6e959adba86654c7b7b1ab7
Xi SONG <sungxisx@icloud.com>
2020-12-01 16:20:21 +0800

    LeetCode 34

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode34.java
@@ -0,0 +1,31 @@
+package Algorithm.Solutions.LeetCode;
+
+public class LeetCode34 {
+
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return new int[] {-1, -1};
+        }
+        int l = binarySearch(nums, target, true);
+        int r = binarySearch(nums, target, false) - 1;
+        if (l <= r && r < nums.length && nums[l] == target && nums[r] == target) {
+            return new int[] {l, r};
+        }
+        return new int[] {-1, -1};
+    }
+
+    private int binarySearch(int[] nums, int target, boolean rightFlag) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        int mid;
+        while (left <= right) {
+            mid = (left + right) >> 1;
+            if (nums[mid] > target || (rightFlag && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
