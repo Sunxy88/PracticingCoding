b87e9869e5f71f6e000f8e59726d4aa852ea3560
Xi SONG <sungxisx@icloud.com>
2021-01-12 16:33:18 +0100

    LeetCode 1203

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode1203.java
@@ -0,0 +1,98 @@
+package Algorithm.Solutions.LeetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+public class LeetCode1203 {
+
+    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        for (int i = 0; i < group.length; i++) {
+            if (group[i] == -1) {
+                group[i] = m;
+                m++;
+            }
+        }
+
+        List<Integer>[] groupAdj = new ArrayList[m];
+        List<Integer>[] itemAdj = new ArrayList[n];
+        for (int i = 0; i < m; i++) {
+            groupAdj[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            itemAdj[i] = new ArrayList<>();
+        }
+
+        int[] groupsIndegree = new int[m];
+        int[] itemsIndegree = new int[n];
+
+        int len = group.length;
+        for (int i = 0; i < len; i++) {
+            int currentGroup = group[i];
+            for (int beforeItem : beforeItems.get(i)) {
+                int beforeGroup = group[beforeItem];
+                if (beforeGroup != currentGroup) {
+                    groupAdj[beforeGroup].add(currentGroup);
+                    groupsIndegree[currentGroup]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (Integer item : beforeItems.get(i)) {
+                itemAdj[item].add(i);
+                itemsIndegree[i]++;
+            }
+        }
+
+        List<Integer> groupsList = topologicalSort(groupAdj, groupsIndegree, m);
+        if (groupsList.size() == 0) {
+            return new int[0];
+        }
+        List<Integer> itemsList = topologicalSort(itemAdj, itemsIndegree, n);
+        if (itemsList.size() == 0) {
+            return new int[0];
+        }
+
+        Map<Integer, List<Integer>> groups2Items = new HashMap<>();
+        for (Integer item : itemsList) {
+            groups2Items.computeIfAbsent(group[item], key -> new ArrayList<>()).add(item);
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (Integer groupId : groupsList) {
+            List<Integer> items = groups2Items.getOrDefault(groupId, new ArrayList<>());
+            res.addAll(items);
+        }
+        return res.stream().mapToInt(Integer::valueOf).toArray();
+    }
+
+    private List<Integer> topologicalSort(List<Integer>[] adj, int[] inDegree, int n) {
+        List<Integer> res = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            Integer front = queue.poll();
+            res.add(front);
+            for (int successor : adj[front]) {
+                inDegree[successor]--;
+                if (inDegree[successor] == 0) {
+                    queue.offer(successor);
+                }
+            }
+        }
+
+        if (res.size() == n) {
+            return res;
+        }
+        return new ArrayList<>();
+    }
+}
