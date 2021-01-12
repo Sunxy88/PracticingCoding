61e7743d5683602d947a966637a23f201af2f255
Xi SONG <sungxisx@icloud.com>
2021-01-12 19:38:50 +0100

    LeetCode 1195

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode1195.java
@@ -0,0 +1,90 @@
+package Algorithm.Solutions.LeetCode;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.IntConsumer;
+
+public class LeetCode1195 {
+}
+
+class FizzBuzz {
+    private int n;
+    private int i = 1;
+    private final Object lock = new Object();
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+        synchronized (lock) {
+            while (i <= n) {
+                if ((!(i % 5 != 0 && i % 3 == 0))) {
+                    lock.wait();
+                }
+                if ((!(i % 5 != 0 && i % 3 == 0))) {
+                    continue;
+                }
+                printFizz.run();
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+        synchronized (lock) {
+            while (i <= n) {
+                if ((!(i % 5 == 0 && i % 3 != 0))) {
+                    lock.wait();
+                }
+                if ((!(i % 5 == 0 && i % 3 != 0))) {
+                    continue;
+                }
+                printBuzz.run();
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+        synchronized (lock) {
+            while (i <= n) {
+                if ((!(i % 5 == 0 && i % 3 == 0))) {
+                    lock.wait();
+                }
+                if ((!(i % 5 == 0 && i % 3 == 0))) {
+                    continue;
+                }
+                printFizzBuzz.run();
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+        synchronized (lock) {
+            while (i <= n) {
+                if (i % 5 == 0 || i % 3 == 0) {
+                    lock.wait();
+                }
+                if ((!(i % 5 != 0 && i % 3 != 0))) {
+                    continue;
+                }
+                if (i > n) {
+                    break;
+                }
+                printNumber.accept(i);
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+}
