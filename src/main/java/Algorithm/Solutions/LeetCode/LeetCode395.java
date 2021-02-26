f13dd5d4e74f56a0daa7ef2240452109809efe91
Xi SONG <sungxisx@icloud.com>
2021-02-26 22:03:57 +0100

    LeetCode 395

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode395.java
@@ -0,0 +1,48 @@
+package Algorithm.Solutions.LeetCode;
+
+public class LeetCode395 {
+
+    public int longestSubstring(String s, int k) {
+        int n = s.length();
+        return dfs(s, 0, n - 1, k);
+    }
+
+    private int dfs(String s, int l, int r, int k) {
+        int[] cnt = new int[26];
+        for (int i = l; i <= r; i++) {
+            cnt[s.charAt(i) - 'a'] += 1;
+        }
+
+        char split = 0;
+        for (int i = 0; i < 26; i++) {
+            if (cnt[i] > 0 && cnt[i] < k) {
+                split = (char) (i + 'a');
+                break;
+            }
+        }
+        if (split == 0) {
+            return r - l + 1;
+        }
+
+        int i = l;
+        int res = 0;
+        while (i <= r) {
+            while (i <= r && s.charAt(i) == split) {
+                i++;
+            }
+            if (i > r) {
+                break;
+            }
+            int start = i;
+            while (i <= r && s.charAt(i) != split) {
+                i++;
+            }
+
+            int length = dfs(s, start, i - 1, k);
+            res = Math.max(res, length);
+        }
+
+        return res;
+    }
+
+}
