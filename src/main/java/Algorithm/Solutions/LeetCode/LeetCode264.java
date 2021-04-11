c6176b551adeae99f41de12b0901370e0e4ccaca
Xi SONG <sungxisx@icloud.com>
2021-04-11 19:51:41 +0800

    LeetCode 264

--- src/main/java/Algorithm/Solutions/LeetCode/LeetCode264.java
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode264.java
@@ -1,28 +1,29 @@
 package Algorithm.Solutions.LeetCode;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 public class LeetCode264 {
-    private Ugly u = new Ugly();
     public int nthUglyNumber(int n) {
-        if (n <= 0)
-            return 1;
-        return u.getUgly(n - 1);
-    }
-}
+        int[] factors = {2, 3, 5};
+        Set<Long> visited = new HashSet<>();
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        pq.offer(1L);
+        visited.add(1L);
+        int ugly = 0;
 
-class Ugly {
-    private int[] nums = new int[1690];
-    public Ugly() {
-        int p2 = 0, p3 = 0, p5 = 0;
-        nums[0] = 1;
-        for (int i = 1; i < 1690; i++) {
-            nums[i] = Math.min(Math.min(nums[p2] * 2, nums[p3] * 3), nums[p5] * 5);
-            if (nums[i] == nums[p2] * 2) p2++;
-            if (nums[i] == nums[p3] * 3) p3++;
-            if (nums[i] == nums[p5] * 5) p5++;
+        for (int i = 0; i < n; i++) {
+            long cur = pq.poll();
+            ugly = (int) cur;
+            for (int f : factors) {
+                long next = f * cur;
+                if (visited.add(next)) {
+                    pq.offer(next);
+                }
+            }
         }
-    }
 
-    public int getUgly(int n) {
-        return nums[n];
+        return ugly;
     }
 }
