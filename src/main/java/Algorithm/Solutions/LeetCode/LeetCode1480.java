aeb5b2e9b68585c6cf53d7971ce25b7a7a9d9fdb
Xi SONG <sungxisx@icloud.com>
2021-05-07 22:19:19 +0800

    LeetCode 1480

---
+++ src/main/java/Algorithm/Solutions/LeetCode/LeetCode1480.java
@@ -0,0 +1,15 @@
+package Algorithm.Solutions.LeetCode;
+
+public class LeetCode1480 {
+
+    public int xorOperation(int n, int start) {
+        int ans = start;
+
+        for (int i = 1; i < n; i++) {
+            ans ^= start + 2 * i;
+        }
+
+        return ans;
+    }
+
+}
